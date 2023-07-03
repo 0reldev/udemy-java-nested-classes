32c708b6219b9c53696d5e98cc97ab7374f71df5
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-03 16:26:46 +0200

    Revisiting the burger challenge with inner classes

--- src/main/java/dev/lpa/burger/Meal.java
+++ src/main/java/dev/lpa/burger/Meal.java
@@ -1,9 +1,12 @@
 package dev.lpa.burger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
 
     private double price = 5.0;
-    private Item burger;
+    private Burger burger;
     private Item drink;
     private Item side;
     private double conversionRate;
@@ -14,20 +17,24 @@ public class Meal {
 
     public Meal(double conversionRate) {
         this.conversionRate = conversionRate;
-        burger = new Item("regular", "burger");
+        burger = new Burger("regular");
         drink = new Item("coke", "drink", 1.5);
         System.out.println(drink.name);
         side = new Item("fries", "side", 2.0);
     }
 
     public double getTotal() {
-        double total = burger.price + drink.price + side.price;
+        double total = burger.getPrice() + drink.price + side.price;
         return Item.getPrice(total, conversionRate);
     }
 
     @Override
     public String toString() {
         return "%s%n%s%n%s%n%26s$%.2f".formatted(burger, drink, side, "Total due:", getTotal());
+    }
+
+    public void addToppings(String... selectedToppings) {
+        burger.addToppings((selectedToppings));
     }
 
     private class Item {
@@ -52,6 +59,53 @@ public class Meal {
 
         private static double getPrice(double price, double rate) {
             return price * rate;
+        }
+    }
+
+    private class Burger extends Item {
+
+        private enum Extra {AVOCADO, BACON, CHEESE, KETCHUP, MAYO, MUSTARD, PICKLES;
+            private double getPrice() {
+                return switch (this) {
+                    case AVOCADO -> 1.0;
+                    case BACON, CHEESE -> 1.5;
+                    default -> 0;
+                };
+            }
+        }
+
+        private List<Item> toppings = new ArrayList<>();
+        Burger(String name) {
+            super(name, "burger", 5.0);
+        }
+
+        public double getPrice() {
+            double total = super.price;
+            for (Item topping : toppings) {
+                total += topping.price;
+            }
+            return total;
+        }
+
+        private void addToppings(String... selectedToppings) {
+            for (String selectedTopping : selectedToppings) {
+                try {
+                    Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
+                    toppings.add(new Item(topping.name(), "TOPPING", topping.getPrice()));
+                } catch (IllegalArgumentException ie) {
+                    System.out.println("No topping found for " + selectedTopping);
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder itemized = new StringBuilder(super.toString());
+            for (Item topping : toppings) {
+                itemized.append("\n");
+                itemized.append(topping);
+            }
+            return itemized.toString();
         }
     }
 }
--- src/main/java/dev/lpa/burger/Store.java
+++ src/main/java/dev/lpa/burger/Store.java
@@ -5,12 +5,18 @@ public class Store {
     public static void main(String[] args) {
 
         Meal regularMeal = new Meal();
+        regularMeal.addToppings("Ketchup", "Mayo", "Bacon", "Cheddar");
         System.out.println(regularMeal);
 //        coke
+//        No topping found for Cheddar
 //        burger        regular $5,00
+//        TOPPING        KETCHUP $0,00
+//        TOPPING           MAYO $0,00
+//        TOPPING          BACON $1,50
 //        drink           coke $1,50
 //        side          fries $2,00
-//        Total due:$8,50
+//        Total due:$10,00
+
 
         Meal usRegularMeal = new Meal(0.68);
         System.out.println(usRegularMeal);
