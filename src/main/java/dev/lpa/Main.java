c25bf3a9914b029fe3ef16a8b2452d6fdf73e9b5
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-04 11:10:01 +0200

    Lesson about local classes

--- src/main/java/dev/lpa/Main.java
+++ src/main/java/dev/lpa/Main.java
@@ -89,5 +89,52 @@ public class Main {
 //        Target  10105 Bud      2020
 //        Walmart 10215 Marty    2016
 //        Walmart 10515 Joe      2021
+
+        System.out.println("With Pig Latin Names");
+        addPigLatinName(storeEmployees);
+//        With Pig Latin Names
+//        Marty artyMay Piggy
+//        Meg egMay Piggy
+//        Joe oeJay Piggy
+//        Tom omTay Piggy
+//        Bud udBay Piggy
+    }
+
+    public static void addPigLatinName(List<? extends StoreEmployee> list) {
+
+        String lastName = "Piggy";
+
+        class DecoratedEmployee extends StoreEmployee implements Comparable<DecoratedEmployee> {
+
+            private String pigLatinName;
+            private Employee originalInstance;
+
+            public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+                this.pigLatinName = pigLatinName + " " + lastName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.toString() + " " + pigLatinName;
+            }
+
+            @Override
+            public int compareTo(DecoratedEmployee o) {
+                return pigLatinName.compareTo(o.pigLatinName);
+            }
+        }
+
+        List<DecoratedEmployee> newList = new ArrayList<>(list.size());
+        for (var employee : list) {
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin, employee));
+        }
+
+        newList.sort(null);
+        for (var dEmployee : newList) {
+            System.out.println(dEmployee.originalInstance.getName() + " " + dEmployee.pigLatinName);
+        }
     }
 }
